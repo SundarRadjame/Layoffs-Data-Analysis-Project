0bb6877efdbf69262ee7598827fb09833e70189d
Sundar <sundarradjame@gmail.com>
2026-01-12 20:09:42 +0530

    Update Layoffs Project - EDA.sql

--- Layoffs Project - EDA.sql
+++ Layoffs Project - EDA.sql
@@ -1,5 +1,5 @@
 -- EDA
--- Here we are jsut going to explore the data and find trends or patterns or anything interesting like outliers
+-- Here we are just going to explore the data and find trends or patterns or anything interesting like outliers
 -- normally when you start the EDA process you have some idea of what you're looking for
 -- with this info we are just going to look around and see what we find!
 
